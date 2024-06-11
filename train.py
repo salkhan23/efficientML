2fc3a565140832105f8ea9a04d67817b3b031f88
Salman Khan <salkhan23@gmail.com>
2024-06-10 21:57:53 -0400

    Add vgg model and a traing routine

---
+++ train.py
@@ -0,0 +1,218 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import os
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+
+from vgg import VGG
+from tqdm import tqdm
+
+
+def show_image(img, title=None):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.title(title)
+
+
+def evaluate(model, data_loader, device):
+    model.eval()
+
+    n_samples = 0
+    n_correct = 0
+
+    for inputs, labels in tqdm(data_loader, desc="eval", leave=False,):
+        # Move the data from CPU to GPU
+        inputs = inputs.to(device)
+        targets = labels.to(device)
+
+        # Inference
+        outputs = model(inputs)
+
+        # Convert logits to class indices
+        outputs = outputs.argmax(dim=1)
+
+        # Update metrics
+        n_samples += labels.size(0)
+        n_correct += (outputs == targets).sum()
+
+    return (n_correct / n_samples * 100).item()
+
+
+def save_model(model, best_model_state_dict, save_dir, best_test_acc, n_epochs):
+
+    save_file_name = \
+        f'./{datetime.now().strftime("%d-%m-%Y")}_{model.__class__.__name__}' \
+        f'_net_train_epochs_{n_epochs}_acc_{int(best_test_acc)}.pth'
+
+    os.makedirs(save_dir, exist_ok=True)
+    save_file_path = os.path.join(save_dir, save_file_name)
+    torch.save(best_model_state_dict, save_file_path)
+
+    print(f'Model saved to {save_file_path}')
+
+
+def main(b_size, random_seed=10, data_dir='./data', save_dir='./results_trained_models', n_epochs=10, lr=1e-2):
+
+    np.random.seed(random_seed)
+    torch.random.manual_seed(random_seed)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    transforms_train = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32, 4),
+        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+    ])
+
+    transforms_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+    ])
+
+    train_set = torchvision.datasets.CIFAR10(
+        root=data_dir,
+        train=True,
+        download=True,
+        transform=transforms_train
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_set,
+        batch_size=b_size,
+        shuffle=True,
+        num_workers=12
+    )
+
+    test_set = torchvision.datasets.CIFAR10(
+        root=data_dir,
+        train=False,
+        download=True,
+        transform=transforms_test
+    )
+
+    test_loader = torch.utils.data.DataLoader(
+        test_set,
+        batch_size=b_size,
+        shuffle=False,
+        num_workers=12)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # Debug ---------------------------------------------------------------------------------------
+    # # Display a single Image
+    # disp_img, disp_img_class = train_loader.dataset[0]
+    # show_image(disp_img, classes[disp_img_class])
+    #
+    # # Display a bunch of images
+    # dataiter = iter(train_loader)
+    # images, labels = next(dataiter)  # returns batches
+    # f = plt.figure()
+    # show_image(torchvision.utils.make_grid(images))
+    # f.suptitle(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+
+    # Model ---------------------------------------------------------------------------------------
+    net = VGG()
+    net.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    optimizer = optim.AdamW(net.parameters(), lr=lr, weight_decay=1e-2)
+    # optimizer = optim.Adam(net.parameters(), lr=lr)
+    # optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
+
+    lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+
+    # Training ------------------------------------------------------------------------------------
+    train_start_time = datetime.now()
+
+    best_test_acc = 0
+    best_model = None
+
+    for epoch in range(n_epochs):
+
+        running_loss = 0.0
+        epoch_start_time = datetime.now()
+        n_samples = 0
+        n_correct = 0
+
+        for b_idx, (data, labels) in \
+                tqdm(enumerate(train_loader, 0), total=len(train_loader), desc=f'Epoch {epoch+1}/{n_epochs}'):
+
+            data = data.to(device)
+            labels = labels.to(device)
+
+            model_out = net(data)
+
+            # Compute the loss using the raw outputs and the true labels
+            loss = criterion(model_out, labels)
+
+            # Zero the gradients before backward pass
+            optimizer.zero_grad()
+            # Backward pass: compute the gradients of the loss with respect to model parameters
+            loss.backward()
+            # Update the model parameters based on the computed gradients
+            optimizer.step()
+
+            # Train Acc
+            predictions = torch.argmax(model_out, dim=1)
+            n_samples += labels.size(0)
+            n_correct += (predictions == labels).sum()
+
+            running_loss += loss.item()
+            if b_idx % 2000 == 1999:  # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {b_idx+ 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+        lr_scheduler.step()
+
+        test_acc = evaluate(net, test_loader, device)
+        train_acc = n_correct / n_samples * 100
+        print(
+            f"Epoch {epoch} took {datetime.now()- epoch_start_time}. Train Acc {train_acc:0.2f}, "
+            f"Test Acc {test_acc:0.2f}")
+
+        # Save the model
+        if test_acc > best_test_acc:
+            best_test_acc = test_acc
+            best_model = net.state_dict()
+
+    end_time = datetime.now()
+    total_training_time = end_time - train_start_time
+    print(f"Total Training Time {total_training_time}")
+
+    if best_model:
+        save_model(
+            model=net,
+            best_model_state_dict=best_model,
+            save_dir=save_dir,
+            n_epochs=n_epochs,
+            best_test_acc=best_test_acc
+        )
+
+    import pdb
+    pdb.set_trace()
+
+
+if __name__ == "__main__":
+    plt.ion()
+    batch_size = 128
+    data_directory = './data/cifar10'
+
+    if torch.cuda.is_available():
+        print("CUDA is available. Moving model to GPU...")
+    else:
+        print("CUDA is not available. Moving model to CPU...")
+
+    main(
+        b_size=batch_size,
+        data_dir=data_directory,
+        n_epochs=100,
+        lr=1e-2
+    )
---
+++ vgg.py
@@ -0,0 +1,61 @@
+import torch
+from torch import nn
+from collections import OrderedDict, defaultdict
+
+
+class VGG(nn.Module):
+    ARCH = [64, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M']
+    # m = Max Pooling Layer
+
+    def __init__(self) -> None:  # -> Specifies the return type of the function
+        super().__init__()
+
+        layers = []
+        # A default-dict is a type of dictionary provided by the collections module in Python. It is
+        # similar to a regular dictionary, but with a key difference: it provides a default value for
+        # any key that does not exist in the dictionary, instead of raising a KeyError.
+        #
+        # The input parameter is known as a default factory.  The default_factory in a default-dict is
+        # a callable (like a function) that provides default values for missing keys. When you try to
+        # access a key that doesn't exist in the dictionary, the default_factory is called to generate
+        # a default value for that key. Specifying int returns a zero. 0 cannot be specified directly
+        # as it is not callable.
+        #
+        # This is frequently used to count occurrences of items in a list. See the Add function
+        counts = defaultdict(int)
+
+        def add(name: str, layer: nn.Module) -> None:
+            layers.append((f"{name}{counts[name]}", layer))
+            counts[name] += 1
+
+        in_channels = 3
+        for x in self.ARCH:
+            if x != 'M':
+                # conv-bn-relu
+                # 3 = kernel_size
+                add("conv", nn.Conv2d(in_channels, x, 3, padding=1, bias=False))
+                add("bn", nn.BatchNorm2d(x))
+                add("relu", nn.ReLU(True))  # True = Inplace, No xtra mem allocated. replaces input.
+                in_channels = x
+        else:
+            # max pool
+            add("pool", nn.MaxPool2d(2))
+
+        # nn.Sequential can take an OrderedDict to create a sequence of layers with specific names.
+        #  This makes it easier to refer to and debug individual layers. For example, if you want
+        #  to access the first convolutional layer, you can refer to it by its name
+        #  (e.g., model.backbone['conv0']) if the layers are named. If a list was used directly, we would
+        # have to reference the layers by their index which is harder to debug.
+        self.backbone = nn.Sequential(OrderedDict(layers))
+        self.classifier = nn.Linear(512, 10)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # backbone: [N, 3, 32, 32] => [N, 512, 2, 2]
+        x = self.backbone(x)
+
+        # avg pool: [N, 512, 2, 2] => [N, 512]
+        x = x.mean([2, 3])  # torch.mean(x, dimensions)
+
+        # classifier: [N, 512] => [N, 10]
+        x = self.classifier(x)
+        return x
